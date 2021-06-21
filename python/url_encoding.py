e0b71d29a37816dc35ab0f5af10248565e151c48
googio <support@goog.io>
2021-06-21 19:28:01 -0400

    added url_encoding example

---
+++ python/url_encoding.py
@@ -0,0 +1,18 @@
+import requests
+from urllib.parse import urlencode, quote
+
+# set the api key in headers
+headers = {
+    "apikey": "YOUR_API_KEY"
+}
+
+def main():
+    data = {"q": "site:linkedin.com/in john doe", "num": 10, "start": 20, "safe": "active"}
+    query = urlencode(data)
+    url = f"https://api.goog.io/v1/search/{query}"
+    resp = requests.get(url, headers=headers)
+    result = resp.json()
+    print(result)
+
+if __name__ == "__main__":
+    main()
