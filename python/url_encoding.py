f40467436dde242b06f1a1463179250148e31b33
goog.io <59339358+googio@users.noreply.github.com>
2023-01-18 16:42:43 -0500

    Update url_encoding.py

--- python/url_encoding.py
+++ python/url_encoding.py
@@ -9,7 +9,7 @@ headers = {
 def main():
     data = {"q": "site:linkedin.com/in john doe", "num": 10, "start": 20, "safe": "active"}
     query = urlencode(data)
-    url = f"https://api.goog.io/v1/search/{query}"
+    url = f"https://api.serply.io/v1/search/{query}"
     resp = requests.get(url, headers=headers)
     result = resp.json()
     print(result)
