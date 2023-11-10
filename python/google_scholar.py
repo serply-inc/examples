17903c8fd87eb24413f3336125bc7aabd4820180
Serply <59339358+googio@users.noreply.github.com>
2023-11-10 20:56:00 +0000

    updated script to get link, header, and citation

--- python/google_scholar.py
+++ python/google_scholar.py
@@ -1,13 +1,33 @@
+import os
+
 import requests
+from bs4 import BeautifulSoup
+
+API_KEY = os.environ.get("API_KEY")
 
 url = "https://api.serply.io/v1/scholar/q=machine+learning+research"
 
-payload = ""
 headers = {
     "User-Agent": "insomnia/8.4.0",
-    "apikey": "YOU_API_KEY"
+    "apikey": API_KEY
 }
 
-response = requests.request("GET", url, data=payload, headers=headers)
+response = requests.get(url, headers=headers)
 
-print(response.json()['html'])
+html = response.json()['html']
+
+soup = BeautifulSoup(html, "html.parser")
+
+cite_results = []
+
+for result in soup.find_all("div", {"class": "gs_ri"}):
+    header = result.find("h3")
+    link = header.find("a").get("href")
+    citation = result.find("div", {"class": "gs_a"}).find("a").get("href")
+    cite_results.append({
+        "title": header.text,
+        "link": link,
+        "citation": citation
+    })
+
+print(cite_results)
