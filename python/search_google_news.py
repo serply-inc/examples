60a1927018290a80cd38329c2813119f51ec4ddf
Serply <59339358+googio@users.noreply.github.com>
2023-11-13 16:00:17 +0000

    added export news to csv

--- python/search_google_news.py
+++ python/search_google_news.py
@@ -17,4 +17,19 @@ url = f"https://api.serply.io/v1/news/" + urllib.parse.urlencode(query)
 resp = requests.get(url, headers=headers)
 results = resp.json()
 
-print(results)
+
+import csv
+
+f = csv.writer(open("news.csv", "w", newline=''))
+
+# Write CSV Header, If you dont need that, remove this line
+f.writerow(["title", "link", "summary", "source_title", "source_href"])
+
+for entry in results["entries"]:
+    f.writerow([
+        entry['title'], 
+        entry['link'], 
+        entry['summary'],
+        entry['source']["title"],
+        entry['source']["href"],
+    ])
